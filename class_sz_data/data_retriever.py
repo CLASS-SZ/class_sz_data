29e971d40af2a5870f7f917d7233bed63db43be5
borisbolliet <boris.bolliet@gmail.com>
2024-10-02 08:52:56 +0100

    Update data_retriever.py
    do not rdownload if repos are already there!

--- class_sz_data/data_retriever.py
+++ class_sz_data/data_retriever.py
@@ -14,6 +14,14 @@ def get_data_from_class_sz_repo(path_to_local_repo):
     Args:
         path_to_local_repo (str): The local path where the class-sz directories should be copied.
     """
+    # Define the directories we expect to find in the local repository
+    required_dirs = ['class-sz/bbn', 'class-sz/class_sz_auxiliary_files/includes']
+
+    # Check if required directories already exist
+    if all(os.path.exists(os.path.join(path_to_local_repo, subdir)) for subdir in required_dirs):
+        print("Required directories already exist. Skipping download.")
+        return
+
     # GitHub repository URL for the ZIP file
     repo_url = "https://github.com/CLASS-SZ/class_sz/archive/refs/heads/main.zip"
 
